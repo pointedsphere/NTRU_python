1cb6e513b261fda8fc42e5363fa03ae8da38e7b9
Scott Donaldson <scottdonaldson88@gmail.com>
2021-10-27 01:05:22 +0100

    input args types

--- NTRU.py
+++ NTRU.py
@@ -11,17 +11,17 @@ import argparse
 parser = argparse.ArgumentParser(description="An implementation of the NTRU encryption algorithm in python3.")
 parser.add_argument("-G","--Gen",action='store_true',\
                     help="Generate the public and private key files.")
-parser.add_argument("-N","--N",default=503,\
+parser.add_argument("-N","--N",default=503,type=int,\
                     help="The order of the polynomial ring, default 503.")
-parser.add_argument("-p","--p",default=3,\
+parser.add_argument("-p","--p",default=3,type=int,\
                     help="The smallest inverse polynomial modulus, default 3.")
-parser.add_argument("-q","--q",default=256,\
+parser.add_argument("-q","--q",default=256,type=int,\
                     help="The largest inverse polynomial modulus, default 256.")
-parser.add_argument("-df","--df",default=61,\
+parser.add_argument("-df","--df",default=61,type=int,\
                     help="Polynomial f has df 1's and df-1 -1's, default 61.")
-parser.add_argument("-dg","--dg",default=20,\
+parser.add_argument("-dg","--dg",default=20,type=int,\
                     help="Polynomial g has dg 1's and -1's, default 20.")
-parser.add_argument("-d","--d",default=18,\
+parser.add_argument("-d","--d",default=18,type=int,\
                     help="Random obfuscating polynomial has dg 1's and -1's, default 18.")
 args = parser.parse_args()
 
